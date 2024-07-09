5b1b57bd0fcf5bc16144eaef5a72cbc6bfd18de7
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2024-07-09 21:11:28 +0200

    Fixed duplicate Navbar
    Don't have navbar on Home.js and App.js
    Only have on App.js and the other pages but not home or will result in duplicate navbar(2 navs)

--- front/src/components/footer.js
+++ front/src/components/footer.js
@@ -5,13 +5,12 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <h3>Company Name</h3>
-        <p>&copy; 2024 Company Name. All rights reserved.</p>
+        <h3>Kempton Kali Amman Temple</h3>
+        <p>&copy; 2024 Kempton Kali Amman Temple. All rights reserved.</p>
         <ul className="socials">
           <li><a href="#"><i className="fa fa-facebook"></i></a></li>
           <li><a href="#"><i className="fa fa-twitter"></i></a></li>
           <li><a href="#"><i className="fa fa-instagram"></i></a></li>
-          <li><a href="#"><i className="fa fa-linkedin"></i></a></li>
         </ul>
       </div>
     </footer>
--- front/src/components/home.js
+++ front/src/components/home.js
@@ -1,28 +1,12 @@
-// Navbar.js
-import './navbar.css';
-
-
 import React from 'react';
-import { Link } from 'react-router-dom'; // Assuming you're using React Router for navigation
 
-const Navbar = () => {
+const Home = () => {
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <Link to="/" className="nav-links">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/booking" className="nav-links">Booking</Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/about" className="nav-links">About</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <h1>Home Page</h1>
+      {/* Your home page content */}
+    </div>
   );
 };
 
-export default Navbar;
+export default Home;
